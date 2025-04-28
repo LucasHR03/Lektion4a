0e6e92cdadcb6dc004a05dac3cf65840164c0dc2
Lucs Holst Rasmussen <Lucaser0315@gmail.com>
2025-04-28 19:59:38 +0200

    dems

--- Lektion4b/Lektion4b/src/routes/+page.svelte
+++ Lektion4b/Lektion4b/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { goto } from '$app/navigation';
+
+
+	let userName = $state('');
+	let password = $state('');
+
+	const btnclick = () => {
+		if (userName === 'admin' && password === 'admin') {
+			goto('/home');
+		} else {
+			alert('Login failed');
+		}
+	};
+</script>
+
+<div>
+	<input class="input m-2" bind:value={userName} placeholder="Brugernavn" />
+</div>
+<div>
+	<input class="input m-2" bind:value={password} placeholder="Kodeord" />
+</div>
+<div>
+	<button class="btn btn-primary m-2" onclick={btnclick}  >Login</button>
+</div>
+
+
---
+++ Lektion4b/Lektion4b/src/routes/login/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+        let username = $state('user');
+        let password = $state('password');
+
+        const login = async () => {
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        });
+        const data = await response.json();
+        if (response.ok) {
+            goto('/admin');
+        } else {
+            alert('Login fejlet!');
+        }
+    };
+
+</script>
+
+<input bind:value={username} type="text" placeholder="Indtast Email" />
+<input bind:value={password} type="password" placeholder="Indtast Password" />
+<button onclick={login}>login</button>
